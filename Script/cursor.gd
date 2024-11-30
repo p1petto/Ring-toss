fb11bfd638ebd4e8751052c439038feb4fe29b21
p1petto <ksyuha3803@mail.ru>
2024-12-01 00:01:19 +0800

    круговой прогресс бар для курсора

--- Script/cursor.gd
+++ Script/cursor.gd
@@ -5,10 +5,10 @@ extends Node3D
 @export var gaze_threshold: float = 0.1  # Максимальное допустимое отклонение курсора
 @export var gaze_time: float = 1.5  # Время удержания взгляда в секундах
 
-@onready var camera: XRCamera3D = $"../XRCamera3D"
-@onready var raycaster: RayCast3D = $"../XRCamera3D/RayCast3D"
-@onready var cursor: ColorRect = $"../XRCamera3D/ColorRect"
-@onready var world = $"../../"
+@onready var camera: XRCamera3D = $"../"
+@onready var raycaster: RayCast3D = $"../RayCast3D"
+@onready var progress_bar = $TextureProgressBar
+@onready var world = $"../../../"
 @onready var timer = $Timer
 
 var hands_are_employed: bool = false
@@ -23,10 +23,19 @@ func _ready():
 	timer.wait_time = gaze_time
 	timer.one_shot = true
 	timer.connect("timeout", Callable(self, "_on_gaze_timer_timeout"))
+	progress_bar.value = 0
+	progress_bar.max_value = 100
 
-func _process(_delta):
+func _process(delta):
 	var looked_object = get_looked_object()
 	var look_position = get_look_position()
+	
+	# Обновление прогресс бара
+	if timer.time_left > 0:
+		var progress = ((gaze_time - timer.time_left) / gaze_time) * 100
+		progress_bar.value = progress
+	else:
+		progress_bar.value = 0
 	
 	# Если держим предмет, проверяем любую поверхность
 	if hands_are_employed:
@@ -36,9 +45,11 @@ func _process(_delta):
 				current_gazed_object = looked_object
 				last_gaze_position = look_position
 				timer.start()
+				progress_bar.value = 0
 		else:
 			current_gazed_object = null
 			timer.stop()
+			progress_bar.value = 0
 	# Если не держим предмет, проверяем только Rings
 	else:
 		if looked_object and looked_object.is_in_group("Rings"):
@@ -47,9 +58,11 @@ func _process(_delta):
 				current_gazed_object = looked_object
 				last_gaze_position = look_position
 				timer.start()
+				progress_bar.value = 0
 		else:
 			current_gazed_object = null
 			timer.stop()
+			progress_bar.value = 0
 
 func _on_gaze_timer_timeout():
 	if current_gazed_object:
@@ -57,6 +70,7 @@ func _on_gaze_timer_timeout():
 			take_item()
 		elif hands_are_employed:
 			drop_item()
+	progress_bar.value = 0
 
 func get_looked_object() -> Node3D:
 	if raycaster.is_colliding():
