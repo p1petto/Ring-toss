d74d24b34e5ac512feea46afb41277a311f89f66
p1petto <ksyuha3803@mail.ru>
2024-12-12 20:47:12 +0800

    Попадание мяча

--- Script/buscket.gd
+++ Script/buscket.gd
@@ -7,42 +7,24 @@ var ball_on_down = false
 signal ball_collided
 
 func _ready() -> void:
-	print("Scene ready")
-	# Настраиваем таймеры
-	start_delay_timer.one_shot = true  # Важно!
-	duration_timer.one_shot = true     # Важно!
-	start_delay_timer.wait_time = 0.5
-	duration_timer.wait_time = 3.0
-	print("Start timer exists: ", start_delay_timer != null)
-	print("Duration timer exists: ", duration_timer != null)
+	pass
 
 
 func _on_upper_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Ball"):
-		print("Ball entered upper area")
 		ball_on_up = true
 
 func _on_upper_area_body_exited(body: Node3D) -> void:
 	if body.is_in_group("Ball"):
-		print("Ball exited upper area")
 		ball_on_up = false
 
 func _on_lower_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Ball"):
-		print("Ball entered lower area")
 		ball_on_down = true
+		if ball_on_up:
+			ball_collided.emit()
+			print("Попааал")
 
 func _on_lower_area_body_exited(body: Node3D) -> void:
 	if body.is_in_group("Ball"):
-		print("Ball exited lower area")
 		ball_on_down = false
-
-
-func _on_start_delay_timer_timeout() -> void:
-	print("Start delay timer timeout - starting particles")
-	start_delay_timer.stop()
-	duration_timer.start()
-
-func _on_duration_timer_timeout() -> void:
-	print("Duration timer timeout - stopping particles")
-	duration_timer.stop()
--- Script/character_body_3d.gd
+++ Script/character_body_3d.gd
@@ -3,16 +3,21 @@ extends XROrigin3D
 @onready var camera = $XRCamera3D
 @onready var world = $"../"
 @onready var ball = $"../PickableObject"
+@onready var busket1 = $"../StaticBody3D2/Busket"
+@onready var busket2 = $"../StaticBody3D2/Busket2"
 @onready var text_debug = $XRCamera3D/MeshInstance3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var is_zone_2 = false
+var score = 0
 
 const BALL_SCENE = preload("res://Scenes/pickable_ball.tscn")
 
 func _ready():
 	ball.ball_died.connect(create_ball)
+	busket1.ball_collided.connect(add_count)
+	busket2.ball_collided.connect(add_count)
 
 func _physics_process(delta: float) -> void:
 	#if not is_on_floor():
@@ -38,15 +43,14 @@ func create_ball():
 	world.add_child(ball)
 	ball.ball_died.connect(create_ball)
 	
-
+func add_count():
+	text_debug.mesh.text = "Попал"
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("zone2"):
 		is_zone_2 = true
-		text_debug.mesh.text = is_zone_2
 
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area.is_in_group("zone2"):
 		is_zone_2 = false
-		text_debug.mesh.text = is_zone_2
