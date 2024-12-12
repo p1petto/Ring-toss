d1b4a42c3a510f7f77f78d9d9f60bb4b6a0589ce
p1petto <ksyuha3803@mail.ru>
2024-12-12 21:40:11 +0800

    fix: мяч появляется перед камерой

--- Script/character_body_3d.gd
+++ Script/character_body_3d.gd
@@ -8,10 +8,10 @@ extends XROrigin3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SPAWN_DISTANCE = 0.7  # Расстояние от камеры для создания мяча
 var is_zone_2 = false
 var curent_score = 0
 var best_score = 0
-
 const BALL_SCENE = preload("res://Scenes/pickable_ball.tscn")
 
 func _ready():
@@ -20,30 +20,26 @@ func _ready():
 	busket.ball_collided.connect(add_count)
 
 func _physics_process(delta: float) -> void:
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
 	pass
-	
+
 func create_ball():
-	var ball_pos = position + Vector3(0, 1.5, -0.5)
+	# Получаем направление взгляда камеры
+	var camera_forward = -camera.global_transform.basis.z.normalized()
+	
+	# Вычисляем позицию для создания мяча
+	var spawn_position = camera.global_position + (camera_forward * SPAWN_DISTANCE)
+	
+	# Создаем новый мяч
 	var ball = BALL_SCENE.instantiate() as Ball
-	ball.position = ball_pos
+	ball.position = spawn_position
+	
+	# Добавляем мяч в сцену
 	world.add_child(ball)
+	
+	# Подключаем сигналы
 	ball.ball_died.connect(create_ball)
 	ball.ball_fell.connect(clear_score)
-	
+
 func add_count():
 	if is_zone_2:
 		curent_score = curent_score + 2
@@ -57,11 +53,10 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.is_in_group("zone2"):
 		is_zone_2 = true
 
-
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	if area.is_in_group("zone2"):
 		is_zone_2 = false
-		
+
 func clear_score():
 	curent_score = 0
 	text_debug.mesh.text = str(curent_score)
